78e7c2ffbdd582a3c767697ca1ecbe22b1044842
Proacc999 <proacc@mynes.com>
2026-08-12 15:57:40 +0530

    Update header text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">App</h1>
+      <h1 className="text-3xl font-bold underline text-red-300">Thanks For using ABC Template 🙏</h1>
  
     </div>
   )
